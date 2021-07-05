6be67c87b87985f07a34bd9b6819640b6876a5a3
Gregor Keßler <grex1@gmx.de>
2021-07-05 11:16:15 +0200

    init

---
+++ components/FooterElement.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="ui  vertical footer segment" style="margin: 2em 0">
+  <div class="ui center aligned container">
+    <div class="ui items">
+      <div class="item aligned center">
+        <div class="middle aligned content">
+          <div class="header">Questions? Happy to help, over at <a target="_blank" href="https://tenx.tech">TenX</a>!</div>
+        </div>
+      </div>
+      </div>
+  </div>
+</div>
+</template>
+<script>
+
+export default {
+}
+</script>
